a40ed87ac0f9e34a6f2b5819e1e16aa170514b6d
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-04-12 22:04:57 +0900

    [#4] PWA 활용 위한 준비중

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   server: { https: true },
-  plugins: [sveltekit(), mkcert()],
+  plugins: [
+    sveltekit(),
+    mkcert(),
+    VitePWA({
+      registerType: "autoUpdate",
+      useCredentials: true,
+      includeAssets: ["favicon.png"],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "mcu-device",
+        short_name: "mcu-device",
+        description: "mmcn-mcu project device app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/src/assets/favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
